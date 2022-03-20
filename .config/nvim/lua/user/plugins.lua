8a563f798bc8dffd8e3ef958037ffb140a232c16
Abraham Francis <abfr049@gmail.com>
2022-03-20 13:14:06 +0530

    add nvim: add impatient

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "lewis6991/impatient.nvim" -- cache lua modules for better startup time
 
   -- Theme
   use "folke/tokyonight.nvim"
