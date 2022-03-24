d372bea3de29c7320cd81fc72754851ac97e38de
Abraham Francis <abfr049@gmail.com>
2022-03-24 20:32:30 +0530

    update nvim: add textobject for entire buffer

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -47,6 +47,9 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "lewis6991/impatient.nvim" -- cache lua modules for better startup time
 
+  -- textobjects
+  use { "kana/vim-textobj-entire", requires = { "kana/vim-textobj-user" } }
+
   -- Keymaps
   use "folke/which-key.nvim"
 
