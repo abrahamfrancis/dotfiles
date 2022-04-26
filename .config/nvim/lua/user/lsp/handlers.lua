b12fb21919a71fd4991769777ae67fdbec0651dc
Abraham Francis <abfr049@gmail.com>
2022-04-26 18:57:22 +0530

    update nvim: use nvim api for vim.cmd

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -74,7 +74,7 @@ local function lsp_keymaps(bufnr)
   vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   vim.keymap.set("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.keymap.set("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
 end
 
 M.on_attach = function(client, bufnr)
