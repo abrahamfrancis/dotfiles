f02bd15191b768f6757d9d9c9f06a2b35750679f
Abraham Francis <abfr049@gmail.com>
2022-05-07 13:01:18 +0530

    update nvim: remove deprecated options

--- .config/nvim/lua/user/nvimtree.lua
+++ .config/nvim/lua/user/nvimtree.lua
@@ -56,10 +56,6 @@ nvim_tree.setup({
 			enable = true,
 		},
 	},
-	update_to_buf_dir = {
-		enable = true,
-		auto_open = true,
-	},
 	diagnostics = {
 		enable = true,
 		icons = {
@@ -92,7 +88,6 @@ nvim_tree.setup({
 		height = 30,
 		hide_root_folder = true,
 		side = "left",
-		auto_resize = true,
 		mappings = {
 			custom_only = false,
 			list = {
@@ -109,16 +104,5 @@ nvim_tree.setup({
 	trash = {
 		cmd = "trash",
 		require_confirm = true,
-	},
-	quit_on_open = 0,
-	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folders = 0,
-		files = 1,
-		folder_arrows = 1,
-		tree_width = 30,
 	},
 })
