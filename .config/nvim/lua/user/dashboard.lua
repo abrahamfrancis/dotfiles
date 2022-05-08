51a18056134d162b947bd9df186279d9b128941e
Abraham Francis <abfr049@gmail.com>
2022-05-08 19:41:43 +0530

    update nvim: update dashboard

--- .config/nvim/lua/user/dashboard.lua
+++ .config/nvim/lua/user/dashboard.lua
@@ -4,4 +4,16 @@ if not status_ok then
   return
 end
 
-alpha.setup(require("alpha.themes.dashboard").config)
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.buttons.val = {
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+alpha.setup(dashboard.opts)
