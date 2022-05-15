7f75e02c54ce800f2e70eee17d9e47cbca117749
Abraham Francis <abfr049@gmail.com>
2022-05-15 19:35:21 +0530

    update lvim: reset comment continuation

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -211,6 +211,10 @@ lvim.plugins = {
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
+lvim.autocommands.custom_groups = {
+  {
+    "BufWinEnter,BufRead,BufNewFile",
+    "*",
+    "setlocal formatoptions+=c formatoptions+=r formatoptions+=o",
+  }
+}
