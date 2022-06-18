2f4e50ff0669fc80e902f640609c1fc1c7118a00
Abraham Francis <abfr049@gmail.com>
2022-06-18 20:50:41 +0530

    update wezterm: add nightfox theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 
 return {
 	hide_tab_bar_if_only_one_tab = true,
-	color_scheme = "wezterm_tokyonight_night",
-	window_background_opacity = 0.98,
+	color_scheme = "nightfox_wezterm",
+	window_background_opacity = 1.0,
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 14.0,
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
